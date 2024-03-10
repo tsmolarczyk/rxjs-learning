512f43731a159a2df688e5adaf3d54ed18401007
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2024-03-10 19:45:55 +0100

    different observable notification

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,40 @@
 import { Observable } from 'rxjs';
 
-const observable$ = new Observable<string>(subscriber => {
-    console.log('Obserable execution')
+const myObservable$ = new Observable<string>(subscriber => {
+    console.log('myObservable$')
     subscriber.next('Alice');
-    setTimeout(()=> subscriber.next('Ben'),2000)
-    setTimeout(()=>subscriber.next('Charlie') ,4000)
+    subscriber.next('Ben');
+    setTimeout(()=> {
+        subscriber.next('Charlie');
+    },2000);
+
+    setTimeout(()=> {
+        subscriber.error(new Error ('Failure'))
+    }, 4000)
+    
+    return () => {
+        console.log('Teardown')
+    }
+    
 });
 
-const subscription = observable$.subscribe( value => console.log(value))
+console.log('Before subscribe')
+myObservable$.subscribe({
+    next: value => console.log(value),
+    complete: () => console.log('completed'),
+    error: (err) => console.log(err.message)
+});
+myObservable$.subscribe
+console.log('After subscribe')
 
-setTimeout(()=> {
-    console.log('ubsub')
-    subscription.unsubscribe()
-}, 3000)
+// import { Observable } from 'rxjs';
+//
+// const observable$ = new Observable<string>(subscriber => {
+//     console.log('Obserable execution')
+//     subscriber.next('Alice');
+//     setTimeout(()=> subscriber.next('Ben'),2000)
+//     setTimeout(()=>subscriber.next('Charlie') ,4000)
+// });
+//
+// observable$.subscribe( value => console.log('sub1: ', value))
+// observable$.subscribe( value => console.log('sub2: ', value))
